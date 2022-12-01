31aa6d8ae025b95820c76dd45c0034c82b69c1c9
GhifariFebri <94579098+GhifariFebri@users.noreply.github.com>
2022-12-01 10:00:05 +0700

    first commit

---
+++ app/src/main/java/com/example/alp_lesswaste/CheckRestaurantActivity.kt
@@ -0,0 +1,11 @@
+package com.example.alp_lesswaste
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheckRestaurantActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check_restaurant)
+    }
+}
